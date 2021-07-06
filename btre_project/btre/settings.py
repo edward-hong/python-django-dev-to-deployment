0707ad4a9eb1d21dc18afc2ad74514e6eb46fe93
Edward Hong <edward.hong527@gmail.com>
2021-07-06 18:11:03 +1200

    S06L36 Media Folder & Adding Data

--- btre_project/btre/settings.py
+++ btre_project/btre/settings.py
@@ -132,3 +132,7 @@ STATICFILES_DIRS = [os.path.join(BASE_DIR, "btre/static")]
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
+
+# Media Folder Settings
+MEDIA_ROOT = os.path.join(BASE_DIR, "media")
+MEDIA_URL = "/media/"
