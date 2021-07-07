1a74c7dcde72737be2a2946fe99d6bf3d1e8e530
Edward Hong <edward.hong527@gmail.com>
2021-07-07 21:00:18 +1200

    S07L44 Search Form Choices

--- btre_project/listings/views.py
+++ btre_project/listings/views.py
@@ -1,7 +1,8 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from django.core.paginator import EmptyPage, PageNotAnInteger, Paginator
 
 from .models import Listing
+from .choices import price_choices, bedroom_choices, state_choices
 
 
 def index(request):
@@ -17,8 +18,15 @@ def index(request):
 
 
 def listing(request, listing_id):
-    return render(request, "listings/listing.html")
+    listing = get_object_or_404(Listing, pk=listing_id)
+    context = {"listing": listing}
+    return render(request, "listings/listing.html", context)
 
 
 def search(request):
-    return render(request, "listings/search.html")
+    context = {
+        "state_choices": state_choices,
+        "price_choices": price_choices,
+        "bedroom_choices": bedroom_choices,
+    }
+    return render(request, "listings/search.html", context)
